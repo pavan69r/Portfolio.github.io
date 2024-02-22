eb37cb4746d35eceafc323f68dd9f1c4f95a2526
pavan69r <81371214+pavan69r@users.noreply.github.com>
2024-02-22 13:54:20 +0530

    Add files via upload

---
+++ assets/js/popup.js
@@ -0,0 +1,20 @@
+function openPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+    document.getElementById("blurBg").style.display = "block";
+  }
+  
+  // Function to close the login popup
+  function closePopup() {
+    document.getElementById("loginPopup").style.display = "none";
+    document.getElementById("blurBg").style.display = "none";
+  }
+
+  function openPopup1() {
+    document.getElementById("loginPopup1").style.display = "block";
+    document.getElementById("blurBg1").style.display = "block";
+  }
+
+  function closePopup1() {
+    document.getElementById("loginPopup1").style.display = "none";
+    document.getElementById("blurBg1").style.display = "none";
+  }
